a1d9461d4d86ada57044636c166174b97b196322
Tess <tess.mueske@gmail.com>
2024-03-19 12:27:00 +0100

    read me addition

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
+    fetchContent()
+})
+
+function fetchContent(){
     fetch ("json server name here")
     .then(res => res.json())
     .then(data => {
         //do something w this data
     })
-})
+}
