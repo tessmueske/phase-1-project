a888ef15e54fdbc347e2018df093e587e7b38f9b
Tess <tess.mueske@gmail.com>
2024-03-19 11:32:14 +0100

    dom content loaded event listener plus fetch

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch ("json server name here")
+    .then(res => res.json())
+    .then(data => {
+        //do something w this data
+    })
+})
