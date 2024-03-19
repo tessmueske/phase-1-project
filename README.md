4405edb4220e3004e9493687db3274eef17eb073
Tess <tess.mueske@gmail.com>
2024-03-19 16:57:11 +0100

    some data

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # phase-1-project
 final project for phase one - javascript
+
 ~~~~~~~~~~~~~~~~~~~~~~~~~
+
 This is an interactive and informative info sheet on Mediterranean countries! It includes a wealth of basic info such as languages spoken, religions practiced, the country's capital, its population, as well as other additional facts. 
 
 There is also information on what European country colonized each Mediterranean country, if any. Users can use a dropdown to filter by English, French, and Italian colonization. The result will also display the year the Mediterranean country achieved independence from said European country.
--- index.js
+++ index.js
@@ -9,3 +9,121 @@ function fetchContent(){
         //do something w this data
     })
 }
+
+
+
+
+
+
+[
+    {
+        "countryName": "Algeria",
+        "population": 3433432,
+        "capital": "Algiers",
+        "squareMiles": 4234234,
+        "religionsRepresented": ["Christianity", "Islam"],
+        "languagesRepresented": ["Arabic", "English"],
+        "occupiedBy": "England",
+        "yearOfIndependence": 2024
+    },
+    {
+        "countryName": "Morocco",
+        "population": 3433432,
+        "capital": "Casablanca",
+        "squareMiles": 4234234,
+        "religionsRepresented": ["Christianity", "Islam"],
+        "languagesRepresented": ["Arabic", "English"],
+        "occupiedBy": "England",
+        "yearOfIndependence": 2024
+    },
+    {
+        "countryName": "Tunisia",
+        "population": 433432,
+        "capital": "Tunis",
+        "squareMiles": 4234234,
+        "religionsRepresented": ["Christianity", "Islam"],
+        "languagesRepresented": ["Arabic", "English"],
+        "occupiedBy": "England",
+        "yearOfIndependence": 2024
+    },
+    {
+        "countryName": "Libya",
+        "population": 3433432,
+        "capital": "Tripoli",
+        "squareMiles": 4234234,
+        "religionsRepresented": ["Christianity", "Islam"],
+        "languagesRepresented": ["Arabic", "English"],
+        "occupiedBy": "England",
+        "yearOfIndependence": 2024
+    },
+    {
+        "countryName": "Egypt",
+        "population": 3433432,
+        "capital": "Cairo",
+        "squareMiles": 4234234,
+        "religionsRepresented": ["Christianity", "Islam"],
+        "languagesRepresented": ["Arabic", "English"],
+        "occupiedBy": "England",
+        "yearOfIndependence": 2024
+    },
+    {
+        "countryName": "Cyprus",
+        "population": 3433432,
+        "capital": "Nicosia",
+        "squareMiles": 4234234,
+        "religionsRepresented": ["Christianity", "Islam"],
+        "languagesRepresented": ["Arabic", "English"],
+        "occupiedBy": "England",
+        "yearOfIndependence": 2024
+    },
+    {
+        "countryName": "Turkey",
+        "population": 3433432,
+        "capital": "Istanbul",
+        "squareMiles": 4234234,
+        "religionsRepresented": ["Christianity", "Islam"],
+        "languagesRepresented": ["Arabic", "English"],
+        "occupiedBy": "England",
+        "yearOfIndependence": 2024
+    },
+    {
+        "countryName": "Palestine",
+        "population": 3433432,
+        "capital": "Algiers",
+        "squareMiles": 4234234,
+        "religionsRepresented": ["Christianity", "Islam"],
+        "languagesRepresented": ["Arabic", "English"],
+        "occupiedBy": "England",
+        "yearOfIndependence": 2024
+    },
+    {
+        "countryName": "Jordan",
+        "population": 3433432,
+        "capital": "Amman",
+        "squareMiles": 4234234,
+        "religionsRepresented": ["Christianity", "Islam"],
+        "languagesRepresented": ["Arabic", "English"],
+        "occupiedBy": "England",
+        "yearOfIndependence": 2024
+    },
+    {
+        "countryName": "Lebanon",
+        "population": 3433432,
+        "capital": "Beirut",
+        "squareMiles": 4234234,
+        "religionsRepresented": ["Christianity", "Islam"],
+        "languagesRepresented": ["Arabic", "English"],
+        "occupiedBy": "England",
+        "yearOfIndependence": 2024
+    },
+    {
+        "countryName": "Syria",
+        "population": 3433432,
+        "capital": "Algiers",
+        "squareMiles": 34234,
+        "religionsRepresented": ["Christianity", "Islam"],
+        "languagesRepresented": ["Arabic", "English"],
+        "occupiedBy": "England",
+        "yearOfIndependence": 2024
+    }
+]
