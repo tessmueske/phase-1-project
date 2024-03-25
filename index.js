72f34de051dc9f790b252e9e9145d3cb876edb70
Tess <tess.mueske@gmail.com>
2024-03-25 15:21:35 +0000

    dropdown additions

--- index.js
+++ index.js
@@ -66,25 +66,18 @@ function fetchContent() {
         .catch(error => console.error('Error fetching JSON!', error));
 }
 
-
-
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    let dropdowns = document.getElementsByClassName("dropdown-content");
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+    let dropdown = document.getElementById("myDropdown");
+    let button = document.querySelector(".dropbtn");
+    dropdown.classList.toggle("show");
+    if (dropdown.classList.contains("show")) {
+      dropdown.style.backgroundColor = "white"; 
+      dropdown.style.color = "black"
+    } else {
+      button.style.backgroundColor = ""; 
+      dropdown.style.backgroundColor = ""; 
+      dropdown.style.color = ""
   }
 }
+
+//mouseOver event changing it to blue or something
